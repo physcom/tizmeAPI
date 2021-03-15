430434d7bdb1bca5510ec0b829d7d15bfb02a6b8
elaman <physcom93@gmail.com>
2021-03-16 01:42:21 +0600

    Update: local voter search

--- src/main/java/com/example/demo/repository/VoterRepository.java
+++ src/main/java/com/example/demo/repository/VoterRepository.java
@@ -13,7 +13,7 @@ import java.util.List;
 @Transactional(readOnly = true)
 public interface VoterRepository extends JpaRepository<Voter, Long> {
 
-    @Query(value = "select v.first_name as firstName, v.last_name as lastName, v.middle_name as middleName, u.name as uikTitle, t.name as tikTitle from voter v left join uik u on u.id = v.uik_id left join tik t on u.tik_id = t.id where (?1 = '%-%' or upper(v.first_name) LIKE %?1%) and (?2 = '%-%' or upper(v.last_name) LIKE %?2%)  and (?3 = '%-%' or upper(v.middle_name) LIKE %?3%) and (?4 = '%-%' or t.name like %?4%) limit 100", nativeQuery = true)
+    @Query(value = "select v.first_name as firstName, v.last_name as lastName, v.middle_name as middleName, u.name as uikTitle, t.name as tikTitle from voter v left join uik u on u.id = v.uik_id left join tik t on u.tik_id = t.id where (?1 = '%-%' or upper(v.first_name) LIKE %?1%) and (?2 = '%-%' or upper(v.last_name) LIKE %?2%)  and (?3 = '%-%' or upper(v.middle_name) LIKE %?3%) and (?4 = '%-%' or t.name like %?4%) and t.region_id = 2 limit 100", nativeQuery = true)
     List<VoterDTO> findAllByRequest(String firstName, String lastName, String middleName, String tikTitle);
 
     @Query(value = "select v.id from voter v left join uik u on u.id = v.uik_id where (?1 = '%-%' or upper(v.first_name) LIKE %?1%) and (?2 = '%-%' or upper(v.last_name) LIKE %?2%)  and (?3 = '%-%' or upper(v.middle_name) LIKE %?3%) and (?4 = '%-%' or lower(u.name) LIKE %?4%)", nativeQuery = true)
